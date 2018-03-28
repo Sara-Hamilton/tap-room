7d628cb564e020a5f88a98ac539bb505e6d1aaed
dalia ramos and Sara Hamilton <sara@>
2018-03-28 08:59:14 -0700

    add list of kegs and display in html with ngFor loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,12 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() +1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
+
+  kegs: Keg[] = [
+    new Keg('Corona Extra', 'Corona', 4, 4.5),
+    new Keg('Amber Ale', 'Full Sail', 6, 6),
+    new Keg('Cascade Pilsner', 'Full Sail', 6, 6),
+    new Keg('Black Butte Porter', 'Deschutes', 6, 5.2),
+    new Keg('Mirror Pond Pale Ale', 'Deschutes', 6, 5)
+  ]
 }
