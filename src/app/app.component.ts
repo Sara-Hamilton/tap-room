78b094291b6521ee2b2669c9a38e058f3a911d30
Sara Hamilton <sara@indeterminate.net>
2018-03-29 08:27:26 -0700

    move keg list data and html from root component to keg-list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,33 +13,6 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
 
-  kegs: Keg[] = [
-    new Keg('Corona Extra', 'Corona', 4, 4.5),
-    new Keg('Amber Ale', 'Full Sail', 6, 6),
-    new Keg('Cascade Pilsner', 'Full Sail', 6, 6),
-    new Keg('Black Butte Porter', 'Deschutes', 6, 5.2),
-    new Keg('Mirror Pond Pale Ale', 'Deschutes', 6, 5),
-    new Keg('Guiness Draught', 'Guiness', 7, 4.2)
-  ]
-
-  lowKegs: Keg[] = [];
-
-  priceColor(currentKeg) {
-    if (currentKeg.price >= 5) {
-      return "table-warning";
-    } else {
-      return "table-success";
-    }
-  }
-
-  alcoholContentColor(currentKeg) {
-    if (currentKeg.alcoholContent >= 5) {
-      return "text-danger";
-    } else {
-      return "text-secondary";
-    }
-  }
-
   selectedKeg = null;
   addingKeg = null;
   showingLowInventory = null;
--- src/app/keg-list/keg-list.component.ts
+++ src/app/keg-list/keg-list.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Keg } from '../models/keg.model'
 
 @Component({
   selector: 'app-keg-list',
   templateUrl: './keg-list.component.html',
   styleUrls: ['./keg-list.component.css']
 })
-export class KegListComponent implements OnInit {
+export class KegListComponent {
 
-  constructor() { }
+  // constructor() { }
+  //
+  // ngOnInit() {
+  // }
 
-  ngOnInit() {
+  kegs: Keg[] = [
+    new Keg('Corona Extra', 'Corona', 4, 4.5),
+    new Keg('Amber Ale', 'Full Sail', 6, 6),
+    new Keg('Cascade Pilsner', 'Full Sail', 6, 6),
+    new Keg('Black Butte Porter', 'Deschutes', 6, 5.2),
+    new Keg('Mirror Pond Pale Ale', 'Deschutes', 6, 5),
+    new Keg('Guiness Draught', 'Guiness', 7, 4.2)
+  ]
+
+  lowKegs: Keg[] = [];
+
+  priceColor(currentKeg) {
+    if (currentKeg.price >= 5) {
+      return "table-warning";
+    } else {
+      return "table-success";
+    }
+  }
+
+  alcoholContentColor(currentKeg) {
+    if (currentKeg.alcoholContent >= 5) {
+      return "text-danger";
+    } else {
+      return "text-secondary";
+    }
   }
 
 }
