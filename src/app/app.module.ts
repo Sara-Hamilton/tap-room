5c110c85377b889090e8453cec44aedc17da07f4
Sara Hamilton <sara@indeterminate.net>
2018-03-29 09:52:15 -0700

    add edit-keg component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { KegListComponent } from './keg-list/keg-list.component';
+import { EditKegComponent } from './edit-keg/edit-keg.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    KegListComponent
+    KegListComponent,
+    EditKegComponent
   ],
   imports: [
     BrowserModule,
